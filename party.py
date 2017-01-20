e2e286466662e7a9db1ba907ed2fbb8d993df74d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-19 17:58:43 -0800

    Additional Mel logic and multiple unit tests working

--- party.py
+++ party.py
@@ -9,13 +9,70 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
 
-    return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
+    >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+    True
+
+    >>> is_mel('Judith Butler', 'judith@awesome.com')
+    False
+
+    >>> is_mel('Mel Melitpolski', 'judith@awesome.com')
+    True
+
+    >>> is_mel('Judith Butler', 'mel@ubermelon.com')
+    True
+
+    >>> is_mel('MEL', 'meg@ubermelon.com')
+    True
+
+    >>> is_mel('Judith Butler', 'MEL@UBERMELON.COM')
+    True
+    """
+    # lowercase input
+    # list of variations of mels name- check if name mel is IN the list
+    # check if input string == mel's email address
+    name = name.lower()
+    email = email.lower()
+    possible_mels = ['mel', 'melitpolski', 'mel melitpolski']
+    if name in possible_mels or email == "mel@ubermelon.com":
+        return True
+    else:
+        return False
 
 
 def most_and_least_common_type(treats):
-    """Given list of treats, return {most, least} common types."""
+    """Given list of treats, return {most, least} common types.
+
+    >>> treats = [
+    ...     {'type': 'dessert'},
+    ...     {'type': 'dessert'},
+    ...     {'type': 'appetizer'},
+    ...     {'type': 'dessert'},
+    ...     {'type': 'appetizer'},
+    ...     {'type': 'drink'},
+    ... ]
+    >>> most_and_least_common_type(treats)
+    ('dessert', 'drink')
+
+    >>> treats = [{'type': 'dessert'}]
+    >>> most_and_least_common_type(treats)
+    ('dessert', 'dessert')
+
+    >>> treats = [
+    ...     {'type': 'dessert'},
+    ...     {'type': 'dessert'},
+    ...     {'type': 'appetizer'},
+    ...     {'type': 'appetizer'}
+    ... ]
+    >>> most_and_least_common_type(treats)
+    ('dessert', 'dessert')
+
+    >>> treats = []
+    >>> most_and_least_common_type(treats)
+    (None, None)
+
+    """
 
     types = {}
 
